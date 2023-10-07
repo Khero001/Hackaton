140ad12907ac5b5e6ddd90090f4dc8764af3538f
Demian Kheri Lopez Luna <demian.lopez@optimen.com.mx>
2023-10-07 13:34:29 -0600

    asd

---
+++ sonificacion_final.py
@@ -0,0 +1,47 @@
+import cv2
+from midiutil import MIDIFile
+import pygame.mixer
+from time import sleep
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
