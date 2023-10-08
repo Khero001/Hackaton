e29abe35cae3f52b5f1985e9ef916e59411b7356
Demian Kheri Lopez Luna <demian.lopez@optimen.com.mx>
2023-10-07 18:57:46 -0600

    asd

--- sonificacion_final.py
+++ sonificacion_final.py
@@ -21,9 +21,97 @@ def resize_img(image, resize_factor):
         image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
     return image
 
+def hls2note(hsl):
+    note=0
+    h,l,s = hsl[0],hsl[1],hsl[2]
+    #todas en octava 4
+    if(h>=172 or h<=7):
+        note=60 # C
+    elif(h>7 and h<=22):
+        note=61 # C#
+    elif(h>22 and h<=37):
+        note=62 # D
+    elif(h>37 and h<=52):
+        note=63 # D#
+    elif(h>52 and h<=67):
+        note=64 # E
+    elif(h>67 and h<=82):
+        note=65 # F
+    elif(h>82 and h<=97):
+        note=66 # F#
+    elif(h>97 and h<=112):
+        note=67 # G
+    elif(h>112 and h<=127):
+        note=68 # G#
+    elif(h>127 and h<=142):
+        note=69 # L
+    elif(h>142 and h<=157):
+        note=70 # L#
+    elif(h>157 and h<=172):
+        note=71 # B
+
+    if( l>=0 and l<42):
+        note-=36
+    elif( l>=42 and l<85):
+        note-=24
+    elif( l>=85 and l<127):
+        note-=12
+    elif( l>=127 and l<170):
+        note=note
+    elif( l>=170 and l<212):
+        note+=12
+    elif( l>=212 and l<=255):
+        note+=24
+    
+    return note,s
+
+def hls2(hsl):
+    note=0
+    h,l,s = hsl[0],hsl[1],hsl[2]
+    #todas en octava 4
+    if(h>=172 or h<=7):
+        note=60 # C
+    elif(h>7 and h<=22):
+        note=61 # C#
+    elif(h>22 and h<=37):
+        note=62 # D
+    elif(h>37 and h<=52):
+        note=63 # D#
+    elif(h>52 and h<=67):
+        note=64 # E
+    elif(h>67 and h<=82):
+        note=65 # F
+    elif(h>82 and h<=97):
+        note=66 # F#
+    elif(h>97 and h<=112):
+        note=67 # G
+    elif(h>112 and h<=127):
+        note=68 # G#
+    elif(h>127 and h<=142):
+        note=69 # L
+    elif(h>142 and h<=157):
+        note=70 # L#
+    elif(h>157 and h<=172):
+        note=71 # B
+
+    if( l>=0 and l<42):
+        note-=36
+    elif( l>=42 and l<85):
+        note-=24
+    elif( l>=85 and l<127):
+        note-=12
+    elif( l>=127 and l<170):
+        note=note
+    elif( l>=170 and l<212):
+        note+=12
+    elif( l>=212 and l<=255):
+        note+=24
+
+
+
+'''
 def img2matrixNotesInstruments(image_hsl):
     notes=[]
-    insts=[]
 
     for y in range(image_hsl.shape[0]):
         row = []
@@ -31,9 +119,9 @@ def img2matrixNotesInstruments(image_hsl):
 
             hsl_value = ((pixel[0]/255)*180, (pixel[2]/2.55), (pixel[1]/2.55))
             row.append(hsl_value)
-        hsl_matrix.append(row)
+        notes.append(row)
 
-
+'''
 
 
 
@@ -49,7 +137,7 @@ def main():
     image= resize_img(image,420)
     #img a color hls
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
-
+    
 
     while True:
         cv2.imshow('imagen de prueba',image)
